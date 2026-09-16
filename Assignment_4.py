39a809c180cc57ed1ab4dc3472fdfd0c24762576
Hrishikesh Gaikwad <hrishikeshgaikwad521985@gmail.com>
2026-09-16 21:18:32 +0530

    Create Assignment_4.py

---
+++ Assignment_4.py
@@ -0,0 +1,19 @@
+def fibonacci_iterative(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = 0, 1
+
+    for _ in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+
+    return b
+
+
+n = int(input("Enter the value of n: "))
+
+print("The", n, "th Fibonacci number is:", fibonacci_iterative(n))
